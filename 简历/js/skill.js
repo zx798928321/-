73b30a4aacb0ed665eb4de893f7e04a261679ef8
zx798928321 <32585742+zx798928321@users.noreply.github.com>
2017-11-04 15:21:08 +0800

    Add files via upload

---
+++ 简历/js/skill.js
@@ -0,0 +1,44 @@
+$(function () {
+    $('#skill1').radialIndicator({
+        barColor: '#87CEEB',
+        barWidth: 6,
+        initValue: 85,
+        roundCorner: true,
+        percentage: true
+    });
+    $('#skill2').radialIndicator({
+        barColor: '#87CEEB',
+        barWidth: 6,
+        initValue: 65,
+        roundCorner: true,
+        percentage: true
+    });
+    $('#skill3').radialIndicator({
+        barColor: '#87CEEB',
+        barWidth: 6,
+        initValue: 60,
+        roundCorner: true,
+        percentage: true
+    });
+    $('#skill4').radialIndicator({
+        barColor: '#87CEEB',
+        barWidth: 6,
+        initValue: 75,
+        roundCorner: true,
+        percentage: true
+    });
+    $('#skill5').radialIndicator({
+        barColor: '#87CEEB',
+        barWidth: 6,
+        initValue: 65,
+        roundCorner: true,
+        percentage: true
+    });
+    $('#skill6').radialIndicator({
+        barColor: '#87CEEB',
+        barWidth: 6,
+        initValue: 50,
+        roundCorner: true,
+        percentage: true
+    });
+});;
